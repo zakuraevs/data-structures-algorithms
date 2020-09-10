004d6f015aa8bc574d9e5befb2a9abcb92a835fd
zakuraevs <zakuraevs@gmail.com>
2020-09-10 17:35:13 +0300

    Added insertionsort c++

---
+++ algorithms/sorting/insertion-sort copy.cpp
@@ -0,0 +1,14 @@
+void sort(vector<int>& a) {
+	for(int i=1; i < a.size(); i++){
+ 
+		int key = a[i];
+		int j = i-1;
+ 
+		while(j > -1 && a[j] > key) {
+			a[j+1] = a[j];
+ 
+			j--;
+		}
+		a[j+1] = key;
+	}
+}
