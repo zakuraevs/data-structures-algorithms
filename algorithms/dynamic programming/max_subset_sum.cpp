941c8f1fbcd72bb53d7d8ed97ef9f9874e542df6
zakuraevs <zakuraevs@gmail.com>
2021-01-09 09:24:22 +0200

    Optimized previous problem for constant space

--- algorithms/dynamic programming/max_subset_sum.cpp
+++ algorithms/dynamic programming/max_subset_sum.cpp
@@ -20,3 +20,25 @@ int maxSubsetSumNoAdjacent(vector<int> array) {
 		
   return maxes[n];
 }
+
+// Constant space version:
+int maxSubsetSumNoAdjacentCosntantSpace(vector<int> array) {
+  // Write your code here.
+	int n = array.size();
+	if(!n) return 0;
+	
+	// We are going to keep track of solutions to subproblems
+	//vector<int> maxes(n+1, 0);
+	//maxes[1] = array[0];
+	
+	int current_max = array[0];
+	int previous_max = 0;
+	
+	for(int i=1; i<n; i++) {
+		int temp = current_max;
+		current_max = max(previous_max + array[i], current_max);
+		previous_max = temp;
+	}
+		
+  return current_max;
+}
